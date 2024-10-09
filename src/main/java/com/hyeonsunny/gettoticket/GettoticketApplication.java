92628908161c4d10832f134c84d63e3ad784475e
GDana <danmi2857@naver.com>
2024-10-10 03:26:20 +0900

    Initial commit

---
+++ src/main/java/com/hyeonsunny/gettoticket/GettoticketApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonsunny.gettoticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettoticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettoticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyeonsunny/gettoticket/GettoticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonsunny.gettoticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettoticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
