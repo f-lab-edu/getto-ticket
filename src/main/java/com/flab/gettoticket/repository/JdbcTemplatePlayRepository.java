3bd6dacbc9e1ebcf9635cbe49ee88a43c7dde721
GDana <danmi2857@naver.com>
2024-11-27 08:19:18 +0900

    [#6] refactor: text-block 적용, null 체크 수정

--- src/main/java/com/flab/gettoticket/repository/JdbcTemplatePlayRepository.java
+++ src/main/java/com/flab/gettoticket/repository/JdbcTemplatePlayRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -17,52 +18,62 @@ public class JdbcTemplatePlayRepository implements PlayRepository {
     }
 
     @Override
-    public List<String> selectPlayAtList(String goodsId, String startDate, String endDate) {
-        String select = "SELECT play_at FROM play_time";
-        String where = "WHERE goods_id = ? AND play_at BETWEEN ? AND ? GROUP BY play_at ORDER BY play_at";
-        String sql = select + " " + where;
+    public List<LocalDate> selectPlayAtList(long goodsId, LocalDate startDate, LocalDate endDate) {
+        String sql = """
+                    SELECT play_at FROM play_time
+                    WHERE goods_id = ? AND play_at BETWEEN ? AND ?
+                    GROUP BY play_at
+                    ORDER BY play_at
+                    """;
 
-        List<String> list = jdbcTemplate.query(sql, playAtRowMapper(), goodsId, startDate, endDate);
+        List<LocalDate> list = jdbcTemplate.query(sql, playAtRowMapper(), goodsId, startDate, endDate);
 
-        return list.isEmpty() ? null : list;
+        return list;
     }
 
     @Override
-    public List<PlayTime> selectTimeTable(String goodsId, String playAt) {
-        String select = "SELECT play_at, play_order, play_time, id FROM play_time";
-        String where = "WHERE goods_id = ? AND play_at = ? ORDER BY play_order, play_time";
-        String sql = select + " " + where;
+    public List<PlayTime> selectTimeTable(long goodsId, LocalDate playAt) {
+        String sql = """
+                    SELECT play_at, play_order, play_time, id FROM play_time
+                    WHERE goods_id = ? AND play_at = ?
+                    ORDER BY play_order, play_time
+                    """;
 
         List<PlayTime> list = jdbcTemplate.query(sql, timeTableRowMapper(), goodsId, playAt);
 
-        return list.isEmpty() ? null : list;
+        return list;
     }
 
     @Override
-    public List<SeatCountDTO> selectSeatCount(String timeId) {
-        String select = "SELECT b.grade, b.name, count(b.name) AS `count` FROM seat a";
-        String join = "LEFT JOIN zone b ON a.goods_id = b.goods_id AND a.zone_id = b.id";
-        String where = "WHERE a.time_id = ? GROUP BY b.name ORDER BY a.name";
-        String sql = select + " " + join + " " + where;
+    public List<SeatCountDTO> selectSeatCount(long playTimeId) {
+        String sql = """
+                    SELECT b.grade, b.name, count(b.name) AS `count`
+                    FROM seat a
+                    LEFT JOIN zone b
+                    ON a.goods_id = b.goods_id AND a.zone_id = b.id
+                    WHERE a.time_id = ?
+                    GROUP BY b.name
+                    ORDER BY a.name
+                    """;
 
-        List<SeatCountDTO> list = jdbcTemplate.query(sql, seatCountRowMapper(), timeId);
+        List<SeatCountDTO> list = jdbcTemplate.query(sql, seatCountRowMapper(), playTimeId);
 
-        return list.isEmpty() ? null : list;
+        return list;
     }
 
-    private RowMapper<String> playAtRowMapper() {
+    private RowMapper<LocalDate> playAtRowMapper() {
         return ((rs, rowNum) -> {
-            return rs.getString("play_at");
+            return rs.getDate("play_at").toLocalDate();
         });
     }
 
     private RowMapper<PlayTime> timeTableRowMapper() {
         return ((rs, rowNum) -> {
             PlayTime playTime = new PlayTime();
-            playTime.setPlayAt(rs.getString("play_at"));
+            playTime.setPlayAt(rs.getDate("play_at").toLocalDate());
             playTime.setPlayOrder(rs.getInt("play_order"));
             playTime.setPlayTime(rs.getInt("play_time"));
-            playTime.setPlayTimeId(rs.getString("id"));
+            playTime.setPlayTimeId(rs.getLong("id"));
             return playTime;
         });
     }
