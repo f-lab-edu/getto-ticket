20526b6738c5d68bba4163f83de4b2a87d9c7441
GDana <danmi2857@naver.com>
2024-12-30 19:39:05 +0900

    feat: redisson 설정

---
+++ src/main/java/com/flab/gettoticket/config/RedissonConfig.java
@@ -0,0 +1,37 @@
+package com.flab.gettoticket.config;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+
+        // Redis 서버 설정
+        config.useSingleServer()
+                .setAddress(REDISSON_HOST_PREFIX + host + ":" + port)
+                .setPassword(password);
+
+        // RedissonClient 반환
+        return Redisson.create(config);
+    }
+}
